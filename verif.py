0f240b991a3b18f8194fd8148ee65218267db18b
Muhammad Yusuf Khattak <khattmu@myth54.stanford.edu>
2023-12-10 11:47:41 -0800

    add bound checking

--- verif.py
+++ verif.py
@@ -1,54 +1,88 @@
 from pysmt.shortcuts import *
 from pysmt.typing import *
+from z3 import *
+
 
 # create a type of Bit-Vector of size 32.
-BV32 = BVType(32)
+# BV32 = BVType(32)
 
 ### Network architectur
 def network():
-    input_val = Symbol("input", BV32)
+    # Define the function f(x). For example, f(x) = x^2.
+    def f(x):
+        return x**2
 
-    # add actual softmax logic shit here but for now
-    remainder = BVURem(input_val, BV(10, 32))
-    
-    leq_5 = And(BVUGE(remainder, BV(0, 32)), BVULE(remainder, BV(5, 32)))
-    # greater_than_5 = And(GE(remainder, BV(6, 32)), LE(remainder, BV(9, 32)))
-    nearest_odd_multiple = Ite(leq_5, BVAdd(input_val, BVSub(BV(5, 32), remainder)), BVSub(input_val, BVSub(BV(5, 32), remainder)))
-
-    actual_output = BV(25, 32)
-    operations_prop = Equals(actual_output, nearest_odd_multiple)
-    range_prop = And(BVULE(input_val, BVAdd(actual_output, BV(4, 32))), BVUGE(input_val, BVSub(actual_output, BV(5, 32))))
-    
+    # Create a Z3 Solver
     solver = Solver()
-    solver.add_assertion(Or(Not(operations_prop), Not(range_prop)))
-    solver.solve()
 
+    # Define a Real variable
+    x = Real('x')
 
-    """
-    actual transformer
-    
-    define input matrix as symbol
-    we know what the output should be, store in "actual_output" var or smth
+    # Define the bounds
+    lower_bound = RealVal(10)
+    upper_bound = RealVal(20)
 
-    do all the operations
-    property to verify:
-        result of operations is NOT equal to actual_output
-        the input is NOT within the range
-        take disjunction of above, make sure it is unsat
+    # Add constraints to the solver
+    solver.add(f(x) >= lower_bound, f(x) <= upper_bound)
 
-    ask: logic (does the disjunction of negations work? right now it's saying property does
-    not hold for our trivial example)
-    ask: implementation of actual softmax
-        we know we should initialize a matrix, but after that. crickets
-        can we just import onnx and like. run it
-    ask: is there more than one operation bc it looks like a billion
-    
-    """
-    if not solver.solve():
-        print("Property holds for the following model:")
-        print(solver.get_model())
+    # Check if the constraints are satisfiable
+    if solver.check() == sat:
+        print("Solution exists within the bounds.")
+        print("One such solution: ", solver.model())
     else:
-        print("Property does not hold.")
+        print("No solution exists within the bounds.")
+
+
+    ## DEAD CODE BELOW 
+    # input_val = Symbol("input", BV32)
+
+    # # add actual softmax logic shit here but for now
+    # remainder = BVURem(input_val, BV(10, 32))
+    
+    # leq_5 = And(BVUGE(remainder, BV(0, 32)), BVULE(remainder, BV(5, 32)))
+    # # greater_than_5 = And(GE(remainder, BV(6, 32)), LE(remainder, BV(9, 32)))
+    # nearest_odd_multiple = Ite(leq_5, BVAdd(input_val, BVSub(BV(5, 32), remainder)), BVSub(input_val, BVSub(BV(5, 32), remainder)))
+
+    # actual_output = BV(25, 32)
+    # operations_prop = Equals(actual_output, nearest_odd_multiple)
+    # range_prop = And(BVULE(input_val, BVAdd(actual_output, BV(4, 32))), BVUGE(input_val, BVSub(actual_output, BV(5, 32))))
+    
+    # solver = Solver()
+    # solver.add_assertion(Or(Not(operations_prop), Not(range_prop)))
+    # solver.solve()
+
+    # if not solver.solve():
+    #     print("Property holds for the following model:")
+    #     print(solver.get_model())
+    # else:
+    #     print("Property does not hold.")
+
+    # """
+    # actual transformer
+    
+    # define input matrix as symbol
+    # we know what the output should be, store in "actual_output" var or smth
+
+    # do all the operations
+    # property to verify:
+    #     result of operations is NOT equal to actual_output
+    #     the input is NOT within the range
+    #     take disjunction of above, make sure it is unsat
+
+    # ask: logic (does the disjunction of negations work? right now it's saying property does
+    # not hold for our trivial example)
+    # ask: implementation of actual softmax
+    #     we know we should initialize a matrix, but after that. crickets
+    #     can we just import onnx and like. run it
+
+    #      - implement and encode softmax function using exponential encoding pySMT 
+    #      - then it's a question  of whether we want to add another encoding via log exponential (in paper) or some improvement
+
+    #      - if we were to encode sigmoid using bit vector
+         
+    # ask: is there more than one operation bc it looks like a billion
+    
+    # """
 
 if __name__ == '__main__':
     network()
