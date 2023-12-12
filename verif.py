c9b5cbe537eb9ac0ed9ed09a59294b48f70b6d87
aiwelinder <welinder@myth59.stanford.edu>
2023-12-11 22:41:40 -0800

    cleaned up code and fixed proposition logic

--- verif.py
+++ verif.py
@@ -6,7 +6,7 @@ import idx2numpy
 import onnxruntime as ort
 import time
 
-EPSILON_VALS = [0.016, 0.02, 0.024, 0.032]
+EPSILON_VALS = [0.012, 0.016, 0.02, 0.024, 0.032]
 
 # Toy model for how our code with the REAL softmax should generally be architect4ed
 def toy_example():
@@ -25,14 +25,14 @@ def toy_example():
     upper_bound = RealVal(20)
 
     # Add constraints to the solver
-    solver.add(f(x) >= lower_bound, f(x) <= upper_bound)
+    solver.add(Or(Not(f(x) >= lower_bound), Not(f(x) <= upper_bound)))
 
     # Check if the constraints are satisfiable
-    if solver.check() == sat:
-        print("Solution exists within the bounds.")
-        print("One such solution: ", solver.model())
-    else:
+    if solver.check() == unsat:
         print("No solution exists within the bounds.")
+    else:
+        print("A solution exists within the bounds.")
+        print("One such solution: ", solver.model())
 
 # basic softmax
 def softmax(x):
@@ -61,8 +61,8 @@ def extract_biases(onnx_model, layer):
 
 def network(perturb):
     start_time = time.perf_counter()
-    print("RUNNING AND VERIFYING UNSIMPLIFIED NETWORK :: self-attention-mnist-small.onnx")
-    print("EPSILON IS ", perturb)
+    print("Running and verifying simplified network: self-attention-mnist-small.onnx")
+    print("epsilon =", perturb)
     # NOTE: this code is hard-coded to work on the self-attention-mnist-small.onnx file
     TEST_NETWORK = './training/self-attention-mnist-small.onnx'
     PRUNED_NETWORK = './pruned_self-attention-mnist-small.onnx'
@@ -120,143 +120,33 @@ def network(perturb):
 
     # Convert weights into labels
     class_labels_perturbed = np.argmax(networkOutput_Perturbed, axis=1)
-    # see these are of a similar shape
-    # print(class_labels)
-    # print(Y_test)
 
     # Now we do formal methods!!
-
-    # For high-level analysis, keep track of how our model performs on each iter
-    aggregate = {
-        "misclassified": 0,
-        "misclassified_and_in_range": 0,
-        "in_range_and_correct": 0
-    }
-    # solver1 considers across ALL inputs
-    solver1 = Solver()
+    
+    solver = Solver()
     # NOTE: this defines the LABELS not the probabilities
     output_to_check = [RealVal(val) for val in class_labels_perturbed]
     expected_output = [RealVal(val) for val in Y_test]
 
     # iterating through each input image
     for i in range(len(expected_output)):
-        # solver2 helps us parse how our logic holds
-        solver2 = Solver()
         # Prop to check if labels are equal
         correct_classification = output_to_check[i] == expected_output[i]
-        solver1.add(correct_classification)
-        solver2.add(correct_classification)
-        # Check if for this image we classified it correctly
-        if solver2.check() != sat:
-            # print("Misclassified!")
-            aggregate["misclassified"] += 1
-        else:
-            # Prop to check if probability arrays are within range of epsilon
-            perturbed_probabilities = [RealVal(val) for val in networkOutput_Perturbed[i]]
-            unperturbed_probabilities = [RealVal(val) for val in networkOutput_UnPerturbed[i]]
-            for j in range(len(perturbed_probabilities)):
-                lower_bound = perturbed_probabilities[j] >= unperturbed_probabilities[j] - perturb
-                upper_bound = perturbed_probabilities[j] <= unperturbed_probabilities[j] + perturb
-                solver2.add(Or(Not(lower_bound), Not(upper_bound)))
-            # erm not sure if this is right...
-            if solver2.check() == sat:
-                # print("Input " + i + " is misclassified as the probabilities were out of range")
-                aggregate["misclassified_and_in_range"] += 1
-            else:
-                # print("There is no way you can be within that range and not give the correct output")
-                aggregate["in_range_and_correct"] += 1
+        # Prop to check if probability arrays are within range of epsilon
+        perturbed_probabilities = [RealVal(val) for val in networkOutput_Perturbed[i]]
+        unperturbed_probabilities = [RealVal(val) for val in networkOutput_UnPerturbed[i]]
+        for j in range(len(perturbed_probabilities)):
+            lower_bound = perturbed_probabilities[j] >= unperturbed_probabilities[j] - perturb
+            upper_bound = perturbed_probabilities[j] <= unperturbed_probabilities[j] + perturb
+            solver.add(Or(Not(And(lower_bound, upper_bound), Not(correct_classification))))
     
-    # Eh why not ..? 
-    print("Checking across ALL the images")
-    if solver1.check() == sat:
-        print("Solution exists within the bounds.")
-        print("One such solution: ", solver1.model())
+    if solver.check() == unsat:
+        print("All images within these bounds are correctly classified.")
     else:
-        print("No solution exists within the bounds.")
-    print("AGGREGATE INFO: ", aggregate)
-    print("TIME TAKEN: ", time.perf_counter() - start_time)
-
-# Buggy as hell
-def simple_network():
-    print("RUNNING AND VERIFYING UNSIMPLIFIED NETWORK :: self-attention-mnist-pgd-small-sim.onnx")
-    # NOTE: 
-    TEST_NETWORK = './aistats_transformer_experiment/training/self-attention-mnist-pgd-small-sim.onnx'
-    PRUNED_NETWORK = './pruned_self-attention-mnist-pgd-small-sim.onnx'
-    onnx_model = onnx.load(TEST_NETWORK)
-    # In order to manually acquire the weights and biases for this particular network, we need to traverse the
-    # network until we find the softmax layer, and store the previous two layers, which contain the weights and bias
-    previous_previous_layer = None
-    previous_layer = None
-    for layer in onnx_model.graph.node:
-        if layer.op_type == 'Softmax' and previous_previous_layer.op_type == 'MatMul':
-            # This runs until we have saved the 2 layers preceding the softmax step
-            # print(previous_previous_layer.op_type)
-            # print(previous_layer.op_type)
-            break
-        previous_previous_layer = previous_layer
-        previous_layer = layer
-    
-    # Now, we have isolated the MatMul (weights), Add (bias), and the Softmax layers
-    weights = extract_weights(onnx_model, previous_previous_layer)
-    # print("Hey it's the weights! ", weights)
-    biases = extract_biases(onnx_model, previous_layer)
-    # print("Hey it's the biases! ", biases)
-
-    # Previously, we tried using the torchvision dataset but instead downloaded it directly and threw it into the repo
-    # mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
-    # X_test = mnist_testset.data.numpy()
-    # Y_test = mnist_testset.targets.numpy()
-    images_file = 'archive/t10k-images-idx3-ubyte/t10k-images-idx3-ubyte'
-    labels_file = 'archive/t10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte'
-    # Load the images and labels from IDX format files
-    X_test = idx2numpy.convert_from_file(images_file)
-    Y_test = idx2numpy.convert_from_file(labels_file)
-    # Load the ONNX model using ONNX Runtime
-    # This network's final 3 layers are pruned s.t. its output is the input to the softmax fn.
-    sess = ort.InferenceSession(PRUNED_NETWORK)
-
-    # Reshape the MNIST image data to the expected input shape of the model
-    input_details = sess.get_inputs()[0]
-    input_name = input_details.name
-    expected_input_shape = input_details.shape
-    # print("Expected input shape:", expected_input_shape)
-    input_data = X_test.astype('float32') / 255
-    input_data = np.reshape(input_data, (input_data.shape[0], 28, 28, 1))  # Change to NHWC format
-    
-    # Run the model up to output name of the Reshape layer right before the MatMul leading to softmax
-    reshape_output_name = sess.get_outputs()[0].name
-
-    for i in range(input_data.shape[0]):
-        single_input = input_data[i:i+1]  # Reshape for single instance
-        output = sess.run([reshape_output_name], {input_name: single_input})
-        # Process the output as needed
-        input_to_matmul = output[0]
-        # Reshape input_to_matmul to match expected weights and compute softmax
-        input_to_matmul = input_to_matmul.reshape(input_to_matmul.shape[0], -1)
-        networkOutput = compute_softmax_values(weights, biases, input_to_matmul)
-        # Convert weights into labels
-        # NOTE: I think this step is wrong, we should be comparing the probabilites not the labels
-        class_labels = np.argmax(networkOutput, axis=1)
-        # Now we do formal methods!!
-        output_to_check = [RealVal(val) for val in class_labels]
-        expected_output = [RealVal(val) for val in Y_test]
-
-    for eps in EPSILON_VALS:
-        solver = Solver()
-        print("Running check for range: ", eps)
-        for j in range(len(output_to_check)):
-            solver.add(output_to_check[j] >= RealVal(expected_output[j] - eps))
-            solver.add(output_to_check[j] <= RealVal(expected_output[j] + eps))
-
-        if solver.check() == sat:
-            print("Solution exists within the bounds.")
-            print("One such solution: ", solver.model())
-        else:
-            print("No solution exists within the bounds.")
-
+        print("There exists an incorrect classification within these bounds.")
+    print("Time taken for verification:", time.perf_counter() - start_time)
+    print("")
 
 if __name__ == '__main__':
-    # toy_example()
     for eps in EPSILON_VALS:
         network(eps)
-    # simple_network()
